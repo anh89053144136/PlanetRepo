a8d1f22232bccf79cda69535ad97c6f34d23357a
anh23 <anh23@rambler.ru>
2018-09-30 19:09:02 +0300

    server part

---
+++ PlanetRepo/Controllers/PlanetsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlanetRepo.Entities;
+
+namespace PlanetRepo.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlanetsController : Controller
+    {
+        private static Planet[] planets = new Planet[] {
+            new Planet() { id= 1, name= "Mercury", lastVisitDate= new DateTime(2004, 0, 1), radius= 2439.7 },
+            new Planet() { id= 2, name= "Venus", lastVisitDate= new DateTime(1970, 0, 1), radius= 6051.8 },
+            new Planet() { id= 3, name= "Earth", lastVisitDate= null, radius= 6371.8 },
+            new Planet() { id= 4, name= "Mars", lastVisitDate= new DateTime(1980, 0, 1), radius= 3389.5 },
+            new Planet() { id= 5, name= "Saturn", lastVisitDate= new DateTime(2009, 7, 11), radius= 58232 },
+            new Planet() { id= 6, name= "Jupiter", lastVisitDate= new DateTime(1997, 9, 15), radius= 69911 },
+            new Planet() { id= 7, name= "Uranus", lastVisitDate= new DateTime(1986, 0, 17), radius= 0 },
+            new Planet() { id= 8, name= "Neptune", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 9, name= "PSR 1257+12", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 10, name= "GJ 1214 b", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 11, name= "Kepler-10 b", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 12, name= "Gliese 667 Cc", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 13, name= "Gliese 581 d", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 14, name= "Gliese 581 g", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 15, name= "Kepler-20 e", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 16, name= "HD 85512 b", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 17, name= "Kepler-22 b", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 18, name= "GD 66 b", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 },
+            new Planet() { id= 19, name= "HD 188753 Ab", lastVisitDate= new DateTime(1989, 7, 1), radius= 24622 }
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<Planet> List()
+        {
+            return planets;
+        }
+    }
+}
---
+++ PlanetRepo/Entities/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanetRepo.Entities
+{
+    public class Planet
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public DateTime? lastVisitDate { set; get; }
+        public double radius { set; get; }
+    }
+}
