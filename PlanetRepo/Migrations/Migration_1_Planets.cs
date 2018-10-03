96757e32382ad86aaca5dad493da0b17fddb45fb
ivan.stepanov <ivan.stepanov.gdc@ts.fujitsu.com>
2018-10-03 17:53:46 +0300

    migrations

---
+++ PlanetRepo/Migrations/Migration_1_Planets.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using FluentMigrator.Infrastructure;
+
+namespace PlanetRepo.Migrations
+{
+    public class PlanetsMigration : Migration
+    {
+        private const string tabPlanets = "Planets";
+
+        public override void Up()
+        {
+            Create.Table(tabPlanets)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("name").AsString()
+                .WithColumn("lastVisitDate").AsDateTime()
+                .WithColumn("radius").AsDouble();
+        }
+
+        public override void Down()
+        {
+            Delete.Table(tabPlanets);
+        }
+    }
+}
---
+++ PlenetRepoTests/DBCreator.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlenetRepoTests
+{
+    [TestClass]
+    public class DBCreator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSQLite()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=test.db")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(PlanetRepo.Program).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </sumamry>
+        private void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
