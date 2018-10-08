89e95fc52a78969181352c486bcf38c156a6ec7f
ivan.stepanov <ivan.stepanov.gdc@ts.fujitsu.com>
2018-10-08 18:12:31 +0300

    nhibernate config

--- PlanetRepo/Infrastructure/NHibernateHelper.cs
+++ PlanetRepo/Infrastructure/NHibernateHelper.cs
@@ -2,6 +2,8 @@
 using System.Web;
 using NHibernate;
 using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
 
 namespace PlanetRepo.Infrastructure
 {
@@ -11,13 +13,27 @@ namespace PlanetRepo.Infrastructure
         private static ISessionFactory _sessionFactory;
         private Microsoft.AspNetCore.Http.HttpContext context;
         
-        public int id { set; get; }
+        public string id {
+            get
+            {
+                return (context.Items[CurrentSessionKey] as ISession).ToString();
+            }
+        }
 
         public NHibernateHelper(Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor)
         {
-            _sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration.DataBaseIntegration(db =>
+            {
+                db.ConnectionString = @"Server=.;Database=PlanetRepo;User Id=PlanetRepoUser;Password=123;";
+                db.Dialect<MsSql2008Dialect>();
+                db.Driver<Sql2008ClientDriver>();
+            });
+            //.AddMapping(domainMapping);
+            configuration.SessionFactory().GenerateStatistics();
+
+            _sessionFactory = configuration.BuildSessionFactory();
             this.context = httpContextAccessor.HttpContext;
-            id = new Random().Next(1, 10);
         }
 
         public ISession GetCurrentSession()
